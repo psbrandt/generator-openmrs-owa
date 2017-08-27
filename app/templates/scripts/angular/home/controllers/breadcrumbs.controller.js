e131dd9b939b1f9a61b0a9f6a4faf974bfc681b6
Ankit kumar <rajadelhi94@gmail.com>
2017-08-28 04:09:01 +0530

    Added patient create component (#31)

--- app/templates/scripts/angular/home/controllers/breadcrumbs.controller.js
+++ app/templates/scripts/angular/home/controllers/breadcrumbs.controller.js
@@ -2,9 +2,7 @@ class BreadCrumbsController {
   constructor() {
     var vm = this;
     vm.links = {};
-    vm.links["OpenMRS Demo Client App"] = "/";
-    vm.links["Demo1"] = "";
-    vm.links["Demo2"] = "";
+    vm.links["Home Components"] = "";
   }
 }
 
